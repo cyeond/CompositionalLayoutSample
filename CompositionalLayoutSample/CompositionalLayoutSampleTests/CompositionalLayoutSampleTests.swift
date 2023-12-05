5a36eaf64c11f5c461e7b44d940be7ff73ccc98f
cyeond <cyeond@gmail.com>
2023-12-05 10:11:55 +0900

    initial commit

---
+++ CompositionalLayoutSample/CompositionalLayoutSampleTests/CompositionalLayoutSampleTests.swift
@@ -0,0 +1,21 @@
+//
+//  CompositionalLayoutSampleTests.swift
+//  CompositionalLayoutSampleTests
+//
+//  Created by YD on 12/5/23.
+//
+
+import XCTest
+@testable import CompositionalLayoutSample
+
+final class CompositionalLayoutSampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+}
